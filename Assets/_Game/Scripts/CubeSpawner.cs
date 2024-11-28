7798fc40a41fcf0d397836e9798f6feba344011b
Muhammed Efdal Şener <m.efdal66@hotmail.com>
2024-11-28 22:46:11 +0300

    unity spawner

---
+++ Assets/_Game/Scripts/CubeSpawner.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    public GameObject SpawnCube;
+    public List<Transform> SpawnPointList;
+    public float spawnrate;
+    public bool canSpawn;
+    int controlSpawn = 0;
+
+    private void Start()
+    {
+        StartCoroutine(spawnerEnumerator());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Q) && !canSpawn)
+        {
+            canSpawn = true;
+            StartCoroutine(spawnerEnumerator());
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            canSpawn = false;
+            StopCoroutine(spawnerEnumerator());
+        }
+    }
+
+    IEnumerator spawnerEnumerator()
+    {
+        while(canSpawn)
+        {
+            int spawnerLocation = Random.Range(0, SpawnPointList.Count);
+            Instantiate(SpawnCube, SpawnPointList[spawnerLocation].position, Quaternion.identity);
+            yield return new WaitForSeconds(spawnrate);
+        }        
+    }
+}
